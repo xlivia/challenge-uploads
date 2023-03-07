4b6508d6111265f720df44888fc957278aa016e2
xlivia <oliviabryanttt@gmail.com>
2023-03-07 14:18:49 -0500

    python challenge commit

--- python-challenge/PyBank/main.py
+++ python-challenge/PyBank/main.py
@@ -5,8 +5,12 @@ import csv
 total_months = 0
 total_price = 0
 average_change = 0.00
-inc_prof = ""
-dec_prof = ""
+greatest_inc = ""
+greatest_dec = ""
+max_profit_price = 0
+min_profit_price = 0
+
+profits = []
 
 # Read Resources/buget_data.csv
 
@@ -19,6 +23,12 @@ with open(fpath, 'r') as csvfile:
         # print(row[0], row[1]) # print each row of csv file
         total_months += 1 # add the total months
         total_price += int(row[1]) # add the total price
+        if (max_profit_price < int(row[1])):
+            max_profit_price = int(row[1])
+            greatest_inc = row[0] + " (" + str(max_profit_price) + ")"
+        if (min_profit_price > int(row[1])):
+            min_profit_price = int(row[1])
+            greatest_dec = row[0] + " (" + str(min_profit_price) + ")"
 
 # Print Outputs
 
@@ -26,6 +36,6 @@ print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {total_months}") # 86
 print(f"Total: ${total_price}") # 22564198
-# print(f"Average Change: ${average_change}") # -8311.11
-# print(f"Greatest Increase in Profits: {inc_prof}") # Aug-16 ($1862002)
-# print(f"Greatest Decrease in Profits: {dec_prof}") # Feb-14 ($-1825558)
+print(f"Average Change: ${average_change}") # -8311.11
+print(f"Greatest Increase in Profits: {greatest_inc}") # Aug-16 ($1862002)
+print(f"Greatest Decrease in Profits: {greatest_dec}") # Feb-14 ($-1825558)
